40ad0541cb650c595226a3dc1e3c26691e597aea
josue589 <josue@calcme.com.br>
2026-07-23 13:29:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    // Essa é a URL do "Porteiro" que você acabou de gerar
     MASTER_URL: "https://script.google.com/macros/s/AKfycbyOaOHT8iyD1mnvAJt2oL0bdEDHLI0vHXDO2kIvYhn-rDIT4sn6ILxia2gJLtv3rDXerw/exec",
     
     IGREJAS: {
         "shalom": {
             nome: "Shalom Music",
-            // COLOQUE AQUI A URL DO APPS SCRIPT ORIGINAL DA SHALOM (Aquela que já estava no seu config.js antigo)
-            url: "https://script.google.com/macros/s/AKfycbxhC5GvuzqpzmPIlM0nircdiWKgXhZZRM4DJ7wGufizZWbRZZBTUV2CrROnBMAkwojJ_g/exec" 
+            url: "https://script.google.com/macros/s/AKfycbxhC5GvuzqpzmPIlM0nircdiWKgXhZZRM4DJ7wGufizZWbRZZBTUV2CrROnBMAkwojJ_g/exec"
         },
         "itc": {
             nome: "Igreja ITC",
-            url:https://script.google.com/macros/s/AKfycbyhaEVoYbLZwGT3rWCFbTBXXNdVLOK_MQgJ9M_KYZMT4-ygqGP_mCh3_fqWXGYoHGOO/exec
+            // Atenção: coloquei uma URL vazia por enquanto para não dar erro.
+            // Quando for usar a ITC, gere a URL do App da Web (terminada em /exec) e cole aqui dentro das aspas.
+            url: ""
         }
     }
 };
