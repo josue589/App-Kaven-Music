13525369cd71a9c72e4720eda4a474a377517f07
josue589 <josue@calcme.com.br>
2026-07-23 11:18:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 const CONFIG = {
-    // Cole a URL que você gerou no Google Apps Script entre as aspas abaixo:
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxhC5GvuzqpzmPIlM0nircdiWKgXhZZRM4DJ7wGufizZWbRZZBTUV2CrROnBMAkwojJ_g/exec"
+    // Aqui você vai cadastrar todas as igrejas clientes no futuro
+    IGREJAS: {
+        "shalom": {
+            nome: "Shalom Music",
+            // Cole aqui a sua URL do Apps Script atual da Shalom
+            url: "https://script.google.com/macros/s/AKfycbxhC5GvuzqpzmPIlM0nircdiWKgXhZZRM4DJ7wGufizZWbRZZBTUV2CrROnBMAkwojJ_g/exec"
+        },
+        "itc": {
+            nome: "Igreja ITC",
+            // Quando criar a planilha da ITC, cole a URL do Apps Script deles aqui
+            url: "URL_DO_APPS_SCRIPT_DA_ITC_AQUI"
+        }
+    }
 };
